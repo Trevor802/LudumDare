234d1094fbfe650c0d9b0c8c9139fa8b730d4c64
Trevor Alex <pascerveau@gmail.com>
2019-10-04 10:21:51 -0600

    add singleton audio manager

---
+++ Assets/Scripts/AudioManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class AudioManager : MonoBehaviour
+{
+    public static AudioManager instance = null;
+
+    public AudioSource musicSource;
+    public AudioSource soundSource;
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else if (instance != this)
+        {
+            Destroy(gameObject);
+        }
+        DontDestroyOnLoad(transform.gameObject);
+    }
+
+    public void PlaySingle(AudioClip clip)
+    {
+        soundSource.clip = clip;
+        soundSource.Play();
+    }
+
+    public void PlayMusic(AudioClip clip)
+    {
+        musicSource.clip = clip;
+        musicSource.Play();
+    }
+}
