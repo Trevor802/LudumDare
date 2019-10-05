41aaaac45bf1b039903baf8d65de867a9d0731d6
Trevor Alex <pascerveau@gmail.com>
2019-10-05 17:18:45 -0600

    add player

---
+++ Assets/Scripts/Actor/Player.cs
@@ -0,0 +1,93 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public int lives = 3;
+    public int steps = 7;
+    private Vector3 initPos;
+    private bool hasKey = false;
+    public LayerMask blockingLayer;
+    private BoxCollider2D boxCollider;
+    private Rigidbody2D rb2D;
+
+    private void Start()
+    {
+        boxCollider = GetComponent<BoxCollider2D>();
+        rb2D = GetComponent<Rigidbody2D>();
+    }
+
+    bool Move(int xDir, int yDir, out RaycastHit2D hit)
+    {
+        Vector2 start = transform.position;
+        Vector2 end = start + new Vector2(xDir, yDir);
+        boxCollider.enabled = false;
+        hit = Physics2D.Linecast(start, end, blockingLayer);
+        boxCollider.enabled = true;
+        if (hit.transform == null)
+        {
+            SmoothMovement(end);
+            return true;
+        }
+        return false;
+    }
+
+    private void SmoothMovement(Vector3 end)
+    {
+        transform.position = end;
+        steps--;
+        if (steps <= 0)
+        {
+            Respawn();
+        }
+    }
+
+    private void Update()
+    {
+        int horizontal = 0;
+        int vertical = 0;
+        horizontal = (int)(Input.GetAxisRaw("Horizontal"));
+        vertical = (int)(Input.GetAxisRaw("Vertical"));
+        if(horizontal != 0)
+        {
+            vertical = 0;
+        }
+
+    }
+
+    public void ResetRespawnPos(Vector3 pos)
+    {
+        initPos = pos;
+    }
+
+    public void Respawn()
+    {
+        transform.position = initPos;
+        lives--;
+        if (lives <=0)
+        {
+            GameOver();
+        }
+    }
+
+    public void GameOver()
+    {
+        Debug.Log("Game Over");
+    }
+
+    public bool TryUseKey()
+    {
+        if (hasKey)
+        {
+            hasKey = false;
+            return true;
+        }
+        return false;
+    }
+
+    public void AddKey()
+    {
+        hasKey = true;
+    }
+}
---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class GameManager : MonoBehaviour
+{
+    #region Singleton
+    public static GameManager instance = null;
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else if (instance != this)
+        {
+            Destroy(gameObject);
+        }
+        DontDestroyOnLoad(transform.gameObject);
+    }
+    #endregion
+    
+}
