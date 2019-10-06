20f74e3d42c47a36916b1383d36b82443e1c0171
Xiubo <zhaoxiubo144@gmail.com>
2019-10-05 19:13:46 -0600

    improve +1s

--- Assets/Scripts/Pickup/Pickups.cs
+++ Assets/Scripts/Pickup/Pickups.cs
@@ -6,7 +6,7 @@ using UnityEngine;
 public class Pickups : MonoBehaviour
 {
     public enum pickup_Type { Key, AP_supply };
-
+    public int AP_supply_qty;
     public pickup_Type pickup;
     public Doors pairedDoor;
     // Start is called before the first frame update
@@ -30,7 +30,7 @@ public class Pickups : MonoBehaviour
         }
         if (collision.gameObject.GetComponent<Player>() && pickup == pickup_Type.AP_supply)
         {
-            collision.gameObject.GetComponent<Player>().AddStep(1);
+            collision.gameObject.GetComponent<Player>().AddStep(AP_supply_qty);
             this.gameObject.SetActive(false);
         }
 
