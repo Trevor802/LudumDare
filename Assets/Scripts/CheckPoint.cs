5cb32b2edf004d02ba8d01c03b14390a8242ec57
tali97 <335271710@qq.com>
2019-10-06 10:44:02 -0600

    modify checkpoints

--- Assets/Scripts/CheckPoint.cs
+++ Assets/Scripts/CheckPoint.cs
@@ -4,21 +4,22 @@ using UnityEngine;
 
 public class CheckPoint : MonoBehaviour
 {
-    private GameMaster gm;
+    //private GameMaster gm;
     //private GameObject playerObj = GameObject.Find("Player");
+    public Vector3 lastCheckPointPos;
     public Player otherPlayer;
     void Start()
     {
-        gm = GameObject.FindWithTag("GM").GetComponent<GameMaster>();
+       // gm = GameObject.FindWithTag("GM").GetComponent<GameMaster>();
     }
     void OnTriggerEnter2D(Collider2D other)
     {
         Player otherPlayer = other.GetComponent<Player>();
         if (otherPlayer.CompareTag("Player"))
         {
-            gm.lastCheckPointPos = transform.position;
+            lastCheckPointPos = transform.position;
             
-            otherPlayer.ResetRespawnPos(gm.lastCheckPointPos);
+            otherPlayer.ResetRespawnPos(lastCheckPointPos);
         }
     }
     void Update()
