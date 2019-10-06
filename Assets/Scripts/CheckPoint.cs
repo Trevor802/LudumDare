3f67012010c0e4022406b669564253013f0683ed
tali97 <335271710@qq.com>
2019-10-05 20:48:54 -0600

    modify the save points

---
+++ Assets/Scripts/CheckPoint.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckPoint : MonoBehaviour
+{
+    private GameMaster gm;
+    //private GameObject playerObj = GameObject.Find("Player");
+    public Player otherPlayer;
+    void Start()
+    {
+        gm = GameObject.FindWithTag("GM").GetComponent<GameMaster>();
+    }
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        Player otherPlayer = other.GetComponent<Player>();
+        if (otherPlayer.CompareTag("Player"))
+        {
+            gm.lastCheckPointPos = transform.position;
+            
+            otherPlayer.ResetRespawnPos(gm.lastCheckPointPos);
+        }
+    }
+    void Update()
+    {
+        
+        
+        
+    }
+}
---
+++ Assets/Scripts/PlayerPos.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerPos : MonoBehaviour
+{
+    private GameMaster gm;
+    public Player playerPos;
+    
+    void Start()
+    {
+        gm = GameObject.FindWithTag("GM").GetComponent<GameMaster>();
+        transform.position = gm.lastCheckPointPos;
+
+    }
+
+
+    void Update()
+    {
+       
+    }
+
+}
