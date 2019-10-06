70f465c0d189778289808048a374aad57034851c
najieyi <nasjieyi@gmail.com>
2019-10-06 10:39:21 -0600

    triggerboard collision fix

--- Assets/Scripts/Tile/Triggerboard.cs
+++ Assets/Scripts/Tile/Triggerboard.cs
@@ -6,10 +6,11 @@ public class Triggerboard : TileNode
 {
     bool Triggered = false;
     bool PlayerDiedOn = false;
+    public string doorName = "door";
+    private float threshold = 0.2f;
     // Start is called before the first frame update
     void Start()
     {
-        
     }
 
     private void OnTriggerEnter2D(Collider2D col)
@@ -22,8 +23,10 @@ public class Triggerboard : TileNode
 
     private void OnTriggerExit2D(Collider2D col)
     {
-        if (PlayerDiedOn)
+        if (!PlayerDiedOn)
+        {
             Triggered = false;
+        }
     }
 
     // Update is called once per frame
@@ -31,19 +34,19 @@ public class Triggerboard : TileNode
     {
         if (Triggered == true)
         {
-            GameObject door = GameObject.Find("door");
+            GameObject door = GameObject.Find(doorName);
             door.GetComponent<BoxCollider2D>().enabled = false;
         }
         if (Triggered == false)
         {
-            GameObject door = GameObject.Find("door");
+            GameObject door = GameObject.Find(doorName);
             door.GetComponent<BoxCollider2D>().enabled = true;
         }
     }
 
     public override void OnPlayerRespawn(Player player)
     {
-        if (Vector2.Distance(player.transform.position, transform.position) < Mathf.Epsilon)
+        if (Vector2.Distance(player.transform.position, transform.position) <= threshold)
         {
             PlayerDiedOn = true;
             Triggered = true;
