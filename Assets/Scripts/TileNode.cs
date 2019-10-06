82a59dd2147e53bf4ff631794576a8c988ad980f
Trevor Alex <pascerveau@gmail.com>
2019-10-05 18:44:06 -0600

    add tilenode parent class

---
+++ Assets/Scripts/TileNode.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class TileNode : MonoBehaviour
+{
+    public abstract void OnTick();
+    public abstract void OnPlayerDead(Player player);
+}
