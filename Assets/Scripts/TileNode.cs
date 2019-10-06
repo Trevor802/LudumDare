d5def13c0405a0545e422ce4876fa40bcbeacc5e
Trevor Alex <pascerveau@gmail.com>
2019-10-06 14:26:02 -0600

    tilenode add onPlayerEnter functions

--- Assets/Scripts/TileNode.cs
+++ Assets/Scripts/TileNode.cs
@@ -4,7 +4,36 @@ using UnityEngine;
 
 public class TileNode : MonoBehaviour
 {
+    protected bool playerIn;
+    protected Player collidedPlayer;
+
     public virtual void OnTickStart() { }
-    public virtual void OnTickEnd() { }
-    public virtual void OnPlayerRespawn(Player player) { }
+    public virtual void OnTickEnd()
+    {
+        if (playerIn)
+        {
+            OnPlayerEnter(collidedPlayer);
+        }
+    }
+    public virtual void OnPlayerRespawn(Player player)
+    {
+        playerIn = false;
+    }
+    public virtual void OnPlayerEnter(Player player) { }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        collidedPlayer = other.GetComponent<Player>();
+        if (collidedPlayer)
+        {
+            playerIn = true;
+        }
+    }
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.GetComponent<Player>() == collidedPlayer)
+        {
+            playerIn = false;
+        }
+    }
 }
