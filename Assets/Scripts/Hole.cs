fc28523b485ae3fc74bf49b8c624a21bb24d9881
Jingyi Zhang <jerryzhangjingyi@gmail.com>
2019-10-06 20:49:47 -0600

    hole animation

--- Assets/Scripts/Hole.cs
+++ Assets/Scripts/Hole.cs
@@ -3,13 +3,21 @@ using System.Collections.Generic;
 using UnityEngine;
 public class Hole : TileNode
 {
+    private Animator animator;
     bool filled = false;
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        //animator.Play("pitOpen");
+    }
     public override void OnPlayerEnter(Player player)
     {
         if (player)
         {
             if (filled == false)
             {
+                print("pass");
+                animator.Play("pitClose");
                 player.Respawn();
             }
             filled = true;
