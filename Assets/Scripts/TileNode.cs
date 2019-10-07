c819e8612a74ed259b1ce741494b76e2c5f28ac3
Trevor Alex <pascerveau@gmail.com>
2019-10-06 20:53:23 -0600

    TileNode add OnPlayerExit() function

--- Assets/Scripts/TileNode.cs
+++ Assets/Scripts/TileNode.cs
@@ -4,41 +4,57 @@ using UnityEngine;
 
 public class TileNode : MonoBehaviour
 {
-    protected bool playerIn;
+    private bool playerInTemp;
+    private bool playerOutTemp;
+    protected bool playerInside;
     protected Player collidedPlayer;
 
     public virtual void OnTickStart() { }
     public virtual void OnTickEnd()
     {
-        if (playerIn)
+        if (playerInTemp)
         {
+            playerInside = true;
             OnPlayerEnter(collidedPlayer);
+            playerInTemp = false;
+        }
+        if (playerOutTemp)
+        {
+            playerInside = false;
+            OnPlayerExit(collidedPlayer);
+            playerOutTemp = false;
         }
     }
     public virtual void OnPlayerRespawnStart(Player player)
     {
-
+        
     }
     public virtual void OnPlayerRespawnEnd(Player player)
     {
-        playerIn = false;
+        playerInTemp = false;
+        playerOutTemp = false;
+        playerInside = false;
     }
 
     public virtual void OnPlayerEnter(Player player) { }
 
+    public virtual void OnPlayerExit(Player player) { }
+
     private void OnTriggerEnter2D(Collider2D other)
     {
         collidedPlayer = other.GetComponent<Player>();
-        if (collidedPlayer)
+        if (collidedPlayer && !playerInside)
         {
-            playerIn = true;
+            playerInTemp = true;
+            playerOutTemp = false;
         }
     }
     private void OnTriggerExit2D(Collider2D other)
     {
-        if (other.GetComponent<Player>() == collidedPlayer)
+        if (collidedPlayer && collidedPlayer == other.GetComponent<Player>() && playerInside)
         {
-            playerIn = false;
+            playerOutTemp = true;
+            playerInTemp = false;
         }
     }
 }
