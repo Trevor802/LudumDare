0dcc76b823ba7012eff7439208ec9b8152bc7c0f
Trevor Alex <pascerveau@gmail.com>
2019-10-07 11:32:38 -0600

    main menu

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void StartAnim()
+    {
+        Debug.Log("Playing Animation");
+    }
+
+    public void OpenLevel()
+    {
+        SceneManager.LoadScene("2DLevel");
+    }
+}
