6516d16a0bf33e580b8b6c8254290d4812670473
Trevor Alex <pascerveau@gmail.com>
2019-10-09 10:45:04 -0600

    fix main menu button animation bug, add mouse enter event

---
+++ Assets/Scripts/MainMenuButton.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class MainMenuButton : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+    private Animator animator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        animator.Play("FunctionButton_Hold");
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        animator.Play("FunctionButton_Idle");
+    }
+}
